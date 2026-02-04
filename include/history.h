52e19faa6fb522e6f203e8efe76c4491f77521e0
finlaysr <60317020+finlaysr@users.noreply.github.com>
2026-02-04 22:37:24 +0000

    Fixed issue where positive history invocation would not calculate the position correctly if overflow had not yet occured

--- include/history.h
+++ include/history.h
@@ -1,9 +1,8 @@
 #include "../include/input.h"
 
-#define HIST_LEN 20
+#define HIST_LEN 8
 
 int check_hist(char *input[INPUT_LEN]);
 void add_hist(char *input[INPUT_LEN]);
-void free_hist();
 void free_hist();
 void print_hist();
--- src/history.c
+++ src/history.c
@@ -6,7 +6,6 @@
 #include <unistd.h>
 
 char *history[HIST_LEN][INPUT_LEN];
-int total = 0;
 int head = 0;
 
 // returns 1 if error in parsing history command, else zero
@@ -24,22 +23,33 @@ int check_hist(char *tokens[INPUT_LEN]) {
     // Determint history number entered
     int pos;
     if (tokens[0][1] == '!') { // If !! entered
-      pos = head;
+      pos = (HIST_LEN + head - 1) % HIST_LEN;
     } else {
       char num_s[4];
       strncpy(num_s, tokens[0] + 1, 3);
       int num = atoi(num_s);
 
-      if (num < -total || num > total || num < -HIST_LEN || num > HIST_LEN ||
-          num == 0) {
-        printf("Invalid history number entered!"); // TODO: Improve this message
+      if (num < -HIST_LEN || num > HIST_LEN) {
+        printf("History index must be from -%d to %d!\n", HIST_LEN, HIST_LEN);
         return 1;
       }
       if (num > 0) {
-        pos = (head + num - 1) % HIST_LEN;
+        if (history[head][0]) {
+          pos = (head + num - 1) % HIST_LEN; // formula if overflow has happened
+        } else {
+          pos = num - 1; // formula if array not overflowed
+        }
+      } else if (num < 0) {
+        pos = (HIST_LEN + head + num) % HIST_LEN; // formula for negative index
       } else {
-        pos = (HIST_LEN + head + num) % HIST_LEN;
+        printf("0 is not a valid history index!\n");
+        return 1;
       }
+    }
+    printf("pos: %d\n", pos);
+    if (history[pos][0] == NULL) { // check the history item is not empty
+      printf("Invalid history entry!\n");
+      return 1;
     }
 
     // Subsitute history element into tokens input
@@ -49,11 +59,12 @@ int check_hist(char *tokens[INPUT_LEN]) {
     return 0;
   }
 
-  add_hist(tokens);
+  add_hist(tokens); // If command was not history invocation add it to history
   return 0;
 }
 
 void add_hist(char *input[INPUT_LEN]) {
+  // Remove old history item if present
   for (int i = 0; history[head][i]; i++) {
     free(history[head][i]);
     history[head][i] = NULL;
@@ -64,17 +75,16 @@ void add_hist(char *input[INPUT_LEN]) {
     strcpy(history[head][i], input[i]);
   }
 
-  total++;
   head = (head + 1) % HIST_LEN;
 }
 
 void print_hist() {
-  int count = 1;
+  int index = 1;
   for (int i = 0; i < HIST_LEN; i++) {
     int pos = (head + i) % HIST_LEN;
     if (history[pos][0]) {
-      printf("%2d: ", count);
-      count++;
+      printf("%2d: ", index);
+      index++;
 
       for (int j = 0; history[pos][j]; j++) {
         printf("%s ", history[pos][j]);
@@ -88,6 +98,7 @@ void free_hist() {
   for (int i = 0; i < HIST_LEN; i++) {
     for (int j = 0; history[i][j]; j++) {
       free(history[i][j]);
+      history[i][j] = NULL;
     }
   }
 }
